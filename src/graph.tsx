0d46ce87264bc8b822d3d0a1895dbeb44275a27a
programmer-alt <modstils88@mail.ru>
2024-04-26 22:14:20 +0400

    modified app.tsx graph.tsx

--- src/graph.tsx
+++ src/graph.tsx
@@ -12,18 +12,31 @@ const Graph: React.FC<Props> = ({ data, width, height, color }) => {
 
   return (
     // 1-div контейнер отвечает за общую область отображения графика
-    <div style={{ width: width, height: height, backgroundColor: '#f0f0f0', padding: 10 }}>
+    <div style={{
+      position: 'absolute', // Добавьте свойство position
+      top: 10, 
+      width: width,
+      height: height,
+      backgroundColor: '#f0f0f0',
+      padding: 10,
+      display: 'flex',
+      //flexDirection: 'column',
+      alignItems: 'flex-end',
+      justifyContent: 'center'
+    }}>
       {data.map((value, index) => (
         // 2-div контейнер отвечает за вертикальное выравнивание столбцов графика
-        <div key={index} style={{ marginBottom: 5, alignItems: 'flex-start' }}>
+        <div key={index} style={{ marginBottom: 2, position: 'relative' }}>
+
           {/* 3-div представляет сам столбец графика */}
           <div style={{
+            flexDirection: 'row',
             backgroundColor: color,
             width: `${(value / maxValue) * 100}%`, // Рассчитываем ширину с учетом соотношения
-            height: height / data.length, // Рассчитываем высоту относительно количества столбцов
+            height: `${(value / maxValue) * height}px`, // Рассчитываем высоту относительно количества столбцов
           }}>
             {/* span отображает текстовое значение столбца */}
-            <span style={{ color: 'white', padding: 5 }}>{value} Bar</span>
+            <span style={{ color: 'white', padding: 10 }}>{value} Bar</span>
           </div>
         </div>
       ))}
