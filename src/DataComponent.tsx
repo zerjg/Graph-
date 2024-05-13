01af6cec4b99cd24f2327459eacd13ee4a5b5a09
programmer-alt <modstils88@mail.ru>
2024-05-13 19:53:48 +0400

    modified graph,dataComponents. added graph.module

--- src/DataComponent.tsx
+++ src/DataComponent.tsx
@@ -7,11 +7,11 @@ const DataComponent: React.FC<DataProps> = ({ data }) => {
   const specifiData = [10,2,12,150,45, 20,5, 30, 50, 15,200, 44, 77,88,120]; 
 
   return (
-    <div className='data' style={{ display: 'flex', alignItems:'center', justifyContent: 'center' }} >
+    <div className='data' style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column', height: '400px' }} >
       
-        <Graph data={specifiData}  width={400} height={200} color='blue'  />
-    
-      
+        <Graph data={specifiData}  width={400} height={200} color='blue' title='График давления шин' />
+        <Graph data={specifiData}  width={300} height={100} color='Lime' title='График скорости' />
+        <Graph data={specifiData}  width={80} height={70} color='SpringGreen' title='График температуры' />
     </div> 
   )
 }
--- src/graph.tsx
+++ src/graph.tsx
@@ -5,51 +5,66 @@ type Props = {
   width: number;
   height: number;
   color: string;
+  title: string;
 };
 
-const Graph: React.FC<Props> = ({ data, width, height, color }) => {
+const Graph: React.FC<Props> = ({
+  data,
+  width,
+  height,
+  color,
+  title,
+}) => {
   const maxValue = Math.max(...data);
   const topContainer = 30;
-  const contentContainer = 30;
-  const bottomAreaContainer = height - topContainer - contentContainer;
-
+  const bottomAreaContainer = 30;
+  const contentContainer = height - topContainer - bottomAreaContainer;
+ 
   return (
-    <div className='graphContainer' style={{
-      position: 'absolute',
-      top: 10,
-      width: width,
-      height: height,
-      backgroundColor: 'white',
-      padding: 10,
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'flex-end',
-      justifyContent: 'center',
-    }}>
-      {data.map((value, index) => (
-        <div className='graphColumnContainer' key={index} style={{ marginBottom: 2, marginRight: 30 }}>
-          <div className='graphColumn' style={{
-            backgroundColor: color,
-            width: width / data.length,
-            height: `${(value / maxValue) * contentContainer}px`,
-          }}>
-          </div>
-          <span style={{ color, padding: 0 }}>{value} Bar</span>
-        </div>
-      ))}
-      <div className="mainContainer" style={{
-        height: height, display: 'flex',
-        flexDirection: 'column',
-        position: 'relative',
-        left: 60
-      }}>
-        <div className="top-container" style={{ height: `${topContainer}px` }}>График показателей в барах
-          <div className="content-container" style={{ height: `${contentContainer}px`, }}> Компания "Luxor"
-            <div className="bottom_area-container" style={{ height: `${bottomAreaContainer}px`, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}> Рассчет </div>
-          </div>
-        </div>
-      </div>
+    <div style={{ display: 'flex', flexDirection: 'column', backgroundColor: 'gold', minHeight: height, width: width }}> {/* это наружний контейнер graphContainer column */}
+      <div style={{ width: '100%', textAlign: 'center', backgroundColor: 'red', minHeight: topContainer }}>{title}</div> {/* этот контейнер для title в него вывести переменную title  */}
+      <div style={{ backgroundColor: '#f0f', minHeight: contentContainer }}></div>{/* этот контейнер для графика в него вывести переменную data */}
+      <div style={{ backgroundColor: '#00ff00', minHeight: bottomAreaContainer }}></div>{/* это  контейнер для подписи для нижнего отступа */}
     </div>
-  );
+  )
+  // return (
+  //   <div className='graphContainer' style={{
+  //     position: 'absolute',
+  //     top: 10,
+  //     width: width,
+  //     height: height,
+  //     backgroundColor: 'white',
+  //     padding: 10,
+  //     display: 'flex',
+  //     flexDirection: 'row',
+  //     alignItems: 'flex-end',
+  //     justifyContent: 'center',
+  //   }}>
+  //     {data.map((value, index) => (
+  //       <div className='graphColumnContainer' key={index} style={{ marginBottom: 2, marginRight: 30 }}>
+  //         <div className='graphColumn' style={{
+  //           backgroundColor: color,
+  //           width: width / data.length,
+  //           height: `${(value / maxValue) * contentContainer}px`,
+  //         }}>
+  //         </div>
+  //         <span style={{ color, padding: 0 }}>{value} Bar</span>
+  //       </div>
+  //     ))}
+  //     <div className="mainContainer" style={{
+  //       height: height, display: 'flex',
+  //       flexDirection: 'column',
+  //       position: 'relative',
+  //       left: 60
+  //     }}>
+  //       <div className="top-container" style={{ height: `${topContainer}px` }}>{title}
+  //         <div className="content-container" style={{ height: `${contentContainer}px`, }}> 
+  //           <div className="bottom_area-container" style={{ height: `${bottomAreaContainer}px`, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}> Рассчет </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
+
 export default Graph; 
